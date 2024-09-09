6abf7928c908ff158e0e73494eaead601b94be5b
fiza <khanfiza3dec004@gmail.com>
2024-09-09 19:42:43 +0530

    CodeLab

--- app/src/main/java/com/example/codelab/movie/character/presentation/state/CharacterState.kt
+++ app/src/main/java/com/example/codelab/movie/character/presentation/state/CharacterState.kt
@@ -1,9 +1,7 @@
 package com.example.codelab.movie.character.presentation.state
 
-import com.example.codelab.movie.character.di.model.Characters
-
 data class CharacterState(
-    val characters:List<Characters> ?= emptyList(),
+    val characters: List<Unit>? = emptyList(),
     val errorMsg:String?="",
     val isLoading:Boolean =false
 )
